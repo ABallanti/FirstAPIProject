d36601d4edfbb591e8f1ce6f7c41287e935d57c2
ABallanti <50780475+ABallanti@users.noreply.github.com>
2025-01-17 21:52:25 +0000

    working with no html/css

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+
+const port = 3000;
+const app = express();
+
+const baseURL = "https://v2.jokeapi.dev";
+const categories = ["Programming", "Christmas"];
+const params = [
+    "blacklistFlags=nsfw,religious,racist,sexist,explicit",
+    "idRange=0-100"
+];
+
+app.get("/",async (req,res) =>{
+    const result = await axios.get (`${baseURL}/joke/${categories.join(",")}?${params.join("&")}`)
+    res.render("index.ejs", { question: result.data.setup, answer: result.data.delivery})
+})
+
+app.listen(port,() =>{
+    console.log(`The server is working in port http://localhost:${port}`)
+})
